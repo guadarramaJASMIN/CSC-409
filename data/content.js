380d777f36714ee49a2fd0cc498a7962799a14bc
guadarramaJASMIN <158100471+guadarramaJASMIN@users.noreply.github.com>
2025-03-18 22:59:45 -0400

    Update content.js

--- data/content.js
+++ data/content.js
@@ -1,1 +1,30 @@
+const contentData = {
+  title: "The Apple Edition: 20 Unforgettable Psych Quotes",
+  logoImage: "images/logo.png", // Update with the actual logo image path
+  description: "Welcome to the best place for Psych lovers! Here are some of the most iconic quotes from the show that are sure to make you SMILE!",
+  quotes: [
+    { quote: "Man, I'm nobody's charity case. I demand I dig my own grave!", character: "Gus" },
+    { quote: "Just because you put syrup on something doesn't make it a pancake.", character: "Shawn" },
+    { quote: "Gus, don't be a gooey chocolate chip cookie.", character: "Shawn" },
+    { quote: "You must be out of your damn mind!", character: "Gus" },
+    { quote: "I can't help it, Shawn, my body craves buttery goodness.", character: "Gus" },
+    { quote: "I'm nobody's pawn. Shawn. I'm a queen.", character: "Gus" },
+    { quote: "Suck it!", character: "Shawn" },
+    { quote: "How can you tell that someone's a compulsive liar? I mean, assuming that their pants aren't on fire.", character: "Shawn" },
+    { quote: "I don't lose things. I place things in locations which later elude me.", character: "Shawn" },
+    { quote: "Injury-free since June, when Gus broke his finger flipping the injury countdown calendar.", character: "Shawn" },
+    { quote: "Well, the plot, unlike your hair, continues to thicken.", character: "Shawn" },
+    { quote: "C'mon Son.", character: "Shawn" },
+    { quote: "Note to self: Be rich one day.", character: "Shawn" },
+    { quote: "It's just that laws keep changing. It's getting very challenging to keep up with them all. For example, did you know that it is now illegal to give a possum a perm?", character: "Shawn" },
+    { quote: "The chips say you're a cheater, cheater pumpkin eater!", character: "Shawn" },
+    { quote: "Remember dad, you treat a woman like a person, then a princess, then a Greek goddess, then a person again.", character: "Shawn" },
+    { quote: "We get caught together, we face death together, it happens every week.", character: "Shawn" },
+    { quote: "I've never been murdered before! It's liberating.", character: "Gus" },
+    { quote: "He can't die in there, Shawn. It's a company car.", character: "Gus" },
+    { quote: "Please. I haven't snuck into your apartment for weeks. Which reminds me, you're all out of peanut butter.", character: "Shawn" }
+  ],
+  footer: "2025 The Pineapple Edition. All rights reserved :)"
+};
 
+export default contentData;
